bface14e751e79803cfdee3fb54abe62b047ea7d
Wesley Nunes <nunes.wesley@hotmail.com>
2017-10-21 18:47:19 -0200

    Aula3

--- typer-inicial/alura-typer/public/js/main.js
+++ typer-inicial/alura-typer/public/js/main.js
@@ -2,4 +2,32 @@ var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase").text(numPalavras);
 
-console.log(tamanhoFrase);
+//console.log(tamanhoFrase);
+
+var campo = $(".campo-digitacao");
+
+campo.on("input",function(){
+      var conteudo =  campo.val();
+
+      var qtdPalavras = conteudo.split("/\s+/").length -1;
+      $("#contador-palavras").text(qtdPalavras);
+
+      var qtdCaracteres = conteudo.length;
+      $("#contador-caracteres").text(qtdCaracteres);
+});
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus",function(){
+    var cronometro = setInterval(function(){
+        
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+
+        if(tempoRestante < 1){
+           campo.attr("disabled",true);
+           clearInterval(cronometro);
+        }
+
+    },1000);    
+
+});
