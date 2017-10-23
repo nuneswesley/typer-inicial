e06607fc00e53139443c2d5079d6904087e50ba6
Wesley Nunes <nunes.wesley@hotmail.com>
2017-10-23 18:04:53 -0200

    Aula4

--- typer-inicial/alura-typer/public/js/main.js
+++ typer-inicial/alura-typer/public/js/main.js
@@ -1,30 +1,55 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase").text(numPalavras);
-
-//console.log(tamanhoFrase);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-campo.on("input",function(){
-      var conteudo =  campo.val();
-     
-      var qtdPalavras = conteudo.split(/\S+/).length -1;
-      $("#contador-palavras").text(qtdPalavras);
-    
-      var qtdCaracteres = conteudo.length;
-      $("#contador-caracteres").text(qtdCaracteres);
+
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometros();
+    $("#botao-reiniciar").click(reiniciaJogo);
+
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus",function(){
-    var cronometro = setInterval(function(){
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase").text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input",function(){
+          var conteudo =  campo.val();
+         
+          var qtdPalavras = conteudo.split(/\S+/).length -1;
+          $("#contador-palavras").text(qtdPalavras);
         
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
+          var qtdCaracteres = conteudo.length;
+          $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
 
-        if(tempoRestante < 1){
-           campo.attr("disabled",true);
-           clearInterval(cronometro);
-        }
-    },1000);    
-});
+function inicializaCronometros() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus",function(){
+        var cronometro = setInterval(function(){
+            
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+    
+            if(tempoRestante < 1){
+               campo.attr("disabled",true);
+               clearInterval(cronometro);
+            }
+        },1000);    
+    });    
+}
+
+function reiniciaJogo() {
+        campo.attr("disabled", false);
+        campo.val("");
+        $("#contador-palavras").text("0");
+        $("#contador-caracteres").text("0");
+        $("#tempo-digitacao").text(tempoInicial);
+        inicializaCronometros();
+}
+
+
